33a1edcd0c6f17c7391364143b64dae1b6df1af2
mchrisbenner <38231509+mchrisbenner@users.noreply.github.com>
2020-07-15 20:07:03 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -6,7 +6,7 @@ request = portal.context.makeRequestRSpec()
 
 # Create a XenVM
 node = request.XenVM("node")
-node.disk_image = "urn:publicid:IDN+emulab.net+img+emulab-ops:UBUNTU18-64-STD"
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
 node.routable_control_ip = "true"
 
 node.addService(rspec.Execute(shell="/bin/sh",
